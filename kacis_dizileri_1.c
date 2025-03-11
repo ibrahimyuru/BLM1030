1cd79c168538bfc24a558714ef1259153522bbcf
İbrahim Yürü <ibrahimyuru@marun.edu.tr>
2025-03-11 17:28:14 +0300

    Create kacis_dizileri_1.c

---
+++ kacis_dizileri_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Merhaba Dünya\n");
+    printf("Tab:    Bu bir tab\n");
+    printf("Çift tırnak \"Merhaba\"\n");
+    printf("Tek tırnak: 'Ahmet'");
+    return 0;
+}
