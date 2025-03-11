e1cdd26bb386130a1a19d1852f238abae75013ba
İbrahim Yürü <ibrahimyuru@marun.edu.tr>
2025-03-11 17:30:09 +0300

    Create printf_kullanim_2.c

---
+++ printf_kullanim_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("%-10s\n", "İbrahim");
+    printf("%10s\n","İbrahim");
+    printf("%5d\n",42);
+    printf("%.3f\n", 3.14159265);
+    printf("%d\n",1+2);
+    printf("%d\n",42-18);
+    printf("%d\n",3*7);
+    printf("%d\n",98/7);
+    printf("%d\n", 25%7)İ
+    return 0;
+}
